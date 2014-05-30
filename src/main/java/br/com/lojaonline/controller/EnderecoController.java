7d7ca40afb629f12404f26b70856ebc6bcea8bf1
RodolfoNaaky <rodolfo.faustino@naaky.com>
2014-05-30 16:59:37 -0300

    Banco Funcionando "testeendereco na views para testar o banco"

--- src/main/java/br/com/lojaonline/controller/EnderecoController.java
+++ src/main/java/br/com/lojaonline/controller/EnderecoController.java
@@ -1,15 +1,5 @@
 package br.com.lojaonline.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import br.com.lojaonline.dao.EnderecoDAO;
-import br.com.lojaonline.model.Endereco;
 
 /*
 
